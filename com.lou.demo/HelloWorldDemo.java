65fb2aaa7c22b6ec621707ae268155e349a5223f
loukq <2475367167@qq.com>
2021-03-13 23:05:18 +0800

    final commit

--- com.lou.demo/HelloWorldDemo.java
+++ com.lou.demo/HelloWorldDemo.java
@@ -1,6 +1,7 @@
 public class HelloWorldDemo {
     public static void main(String[] args) {
         String a="in dev haha";
+        System.out.println("add sometion");
         System.out.println("hello world");
     }
 }
